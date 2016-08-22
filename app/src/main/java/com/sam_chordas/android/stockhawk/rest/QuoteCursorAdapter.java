b9a279494063318e40038787e4d67d082ea0ac79
YSulekha <ysulekha@gmail.com>
2016-08-21 23:23:06 -0700

    Clean the code

--- app/src/main/java/com/sam_chordas/android/stockhawk/rest/QuoteCursorAdapter.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/rest/QuoteCursorAdapter.java
@@ -7,7 +7,6 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,108 +21,112 @@ import com.sam_chordas.android.stockhawk.touch_helper.ItemTouchHelperViewHolder;
 
 /**
  * Created by sam_chordas on 10/6/15.
- *  Credit to skyfishjy gist:
- *    https://gist.github.com/skyfishjy/443b7448f59be978bc59
+ * Credit to skyfishjy gist:
+ * https://gist.github.com/skyfishjy/443b7448f59be978bc59
  * for the code structure
  */
 public class QuoteCursorAdapter extends CursorRecyclerViewAdapter<QuoteCursorAdapter.ViewHolder>
-    implements ItemTouchHelperAdapter{
+        implements ItemTouchHelperAdapter {
 
-  private static Context mContext;
-  private static Typeface robotoLight;
-  private boolean isPercent;
-  private TextView textView ;
-  public QuoteCursorAdapter(Context context, Cursor cursor,TextView view){
-    super(context, cursor, view);
-    textView = view;
-    mContext = context;
-  }
+    private static Context mContext;
+    private static Typeface robotoLight;
+    private boolean isPercent;
+    private TextView textView;
 
-  @Override
-  public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-    robotoLight = Typeface.createFromAsset(mContext.getAssets(), "fonts/Roboto-Light.ttf");
-    View itemView = LayoutInflater.from(parent.getContext())
-        .inflate(R.layout.list_item_quote, parent, false);
-    ViewHolder vh = new ViewHolder(itemView);
-    return vh;
-  }
-
-  @Override
-  public void onBindViewHolder(final ViewHolder viewHolder, final Cursor cursor){
-    viewHolder.symbol.setText(cursor.getString(cursor.getColumnIndex("symbol")));
-    viewHolder.bidPrice.setText(cursor.getString(cursor.getColumnIndex("bid_price")));
-    viewHolder.compName.setText(cursor.getString(cursor.getColumnIndex("name")));
-    int sdk = Build.VERSION.SDK_INT;
-    if (cursor.getInt(cursor.getColumnIndex("is_up")) == 1){
-      if (sdk < Build.VERSION_CODES.JELLY_BEAN){
-        viewHolder.change.setBackgroundDrawable(
-            mContext.getResources().getDrawable(R.drawable.percent_change_pill_green));
-      }else {
-        viewHolder.change.setBackground(
-            mContext.getResources().getDrawable(R.drawable.percent_change_pill_green));
-      }
-    } else{
-      if (sdk < Build.VERSION_CODES.JELLY_BEAN) {
-        viewHolder.change.setBackgroundDrawable(
-            mContext.getResources().getDrawable(R.drawable.percent_change_pill_red));
-      } else{
-        viewHolder.change.setBackground(
-            mContext.getResources().getDrawable(R.drawable.percent_change_pill_red));
-      }
-    }
-    if (Utils.showPercent){
-      viewHolder.change.setText(cursor.getString(cursor.getColumnIndex("percent_change")));
-    } else{
-      viewHolder.change.setText(cursor.getString(cursor.getColumnIndex("change")));
-    }
-  }
-
-  @Override public void onItemDismiss(int position) {
-    Cursor c = getCursor();
-    c.moveToPosition(position);
-    if(position == 0){
-      textView.setText("No Data Available");
-    }
-    String symbol = c.getString(c.getColumnIndex(QuoteColumns.SYMBOL));
-    mContext.getContentResolver().delete(QuoteProvider.Quotes.withSymbol(symbol), null, null);
-    notifyItemRemoved(position);
-    Intent intent = new Intent(StockTaskService.ACTION_DATA_UPDATED);
-    mContext.sendBroadcast(intent);
-  }
-
-  @Override public int getItemCount() {
-    return super.getItemCount();
-  }
-
-  public static class ViewHolder extends RecyclerView.ViewHolder
-      implements ItemTouchHelperViewHolder, View.OnClickListener{
-    public final TextView symbol;
-    public final TextView bidPrice;
-    public final TextView change;
-    public final TextView compName;
-    public ViewHolder(View itemView){
-      super(itemView);
-      symbol = (TextView) itemView.findViewById(R.id.stock_symbol);
-      symbol.setTypeface(robotoLight);
-      bidPrice = (TextView) itemView.findViewById(R.id.bid_price);
-      change = (TextView) itemView.findViewById(R.id.change);
-      compName = (TextView)itemView.findViewById(R.id.name);
-      itemView.setOnClickListener(this);
+    public QuoteCursorAdapter(Context context, Cursor cursor, TextView view) {
+        super(context, cursor, view);
+        textView = view;
+        mContext = context;
     }
 
     @Override
-    public void onItemSelected(){
-      itemView.setBackgroundColor(Color.LTGRAY);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        robotoLight = Typeface.createFromAsset(mContext.getAssets(), "fonts/Roboto-Light.ttf");
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_quote, parent, false);
+        ViewHolder vh = new ViewHolder(itemView);
+        return vh;
     }
 
     @Override
-    public void onItemClear(){
-      itemView.setBackgroundColor(mContext.getResources().getColor(R.color.material_blue_500));
+    public void onBindViewHolder(final ViewHolder viewHolder, final Cursor cursor) {
+        viewHolder.symbol.setText(cursor.getString(cursor.getColumnIndex("symbol")));
+        viewHolder.bidPrice.setText(cursor.getString(cursor.getColumnIndex("bid_price")));
+        viewHolder.compName.setText(cursor.getString(cursor.getColumnIndex("name")));
+        int sdk = Build.VERSION.SDK_INT;
+        if (cursor.getInt(cursor.getColumnIndex("is_up")) == 1) {
+            if (sdk < Build.VERSION_CODES.JELLY_BEAN) {
+                viewHolder.change.setBackgroundDrawable(
+                        mContext.getResources().getDrawable(R.drawable.percent_change_pill_green));
+            } else {
+                viewHolder.change.setBackground(
+                        mContext.getResources().getDrawable(R.drawable.percent_change_pill_green));
+            }
+        } else {
+            if (sdk < Build.VERSION_CODES.JELLY_BEAN) {
+                viewHolder.change.setBackgroundDrawable(
+                        mContext.getResources().getDrawable(R.drawable.percent_change_pill_red));
+            } else {
+                viewHolder.change.setBackground(
+                        mContext.getResources().getDrawable(R.drawable.percent_change_pill_red));
+            }
+        }
+        if (Utils.showPercent) {
+            viewHolder.change.setText(cursor.getString(cursor.getColumnIndex("percent_change")));
+        } else {
+            viewHolder.change.setText(cursor.getString(cursor.getColumnIndex("change")));
+        }
     }
 
     @Override
-    public void onClick(View v) {
-
+    public void onItemDismiss(int position) {
+        Cursor c = getCursor();
+        c.moveToPosition(position);
+        if (position == 0) {
+            textView.setText("No Data Available");
+        }
+        String symbol = c.getString(c.getColumnIndex(QuoteColumns.SYMBOL));
+        mContext.getContentResolver().delete(QuoteProvider.Quotes.withSymbol(symbol), null, null);
+        notifyItemRemoved(position);
+        Intent intent = new Intent(StockTaskService.ACTION_DATA_UPDATED);
+        mContext.sendBroadcast(intent);
     }
-  }
+
+    @Override
+    public int getItemCount() {
+        return super.getItemCount();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder
+            implements ItemTouchHelperViewHolder, View.OnClickListener {
+        public final TextView symbol;
+        public final TextView bidPrice;
+        public final TextView change;
+        public final TextView compName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            symbol = (TextView) itemView.findViewById(R.id.stock_symbol);
+            symbol.setTypeface(robotoLight);
+            bidPrice = (TextView) itemView.findViewById(R.id.bid_price);
+            change = (TextView) itemView.findViewById(R.id.change);
+            compName = (TextView) itemView.findViewById(R.id.name);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onItemSelected() {
+            itemView.setBackgroundColor(Color.LTGRAY);
+        }
+
+        @Override
+        public void onItemClear() {
+            itemView.setBackgroundColor(mContext.getResources().getColor(R.color.material_blue_500));
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
 }
--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -16,45 +16,46 @@ import com.sam_chordas.android.stockhawk.R;
  */
 public class StockIntentService extends IntentService {
 
-  Handler mHandler;
+    Handler mHandler;
 
-  public StockIntentService(){
-    super(StockIntentService.class.getName());
-  }
-
-  public StockIntentService(String name) {
-    super(name);
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    mHandler = new Handler(Looper.getMainLooper());
-  }
-
-  public void updateChart(){
-    mHandler.post(new Runnable() {
-      @Override
-      public void run() {
-
-        Toast toast = Toast.makeText(getBaseContext(),getString(R.string.symbol_not_available),Toast.LENGTH_LONG);
-        toast.show();
-      }
-    });
-  }
-
-  @Override protected void onHandleIntent(Intent intent) {
-    Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
-    StockTaskService stockTaskService = new StockTaskService(this);
-    Bundle args = new Bundle();
-    if (intent.getStringExtra("tag").equals("add")){
-      args.putString("symbol", intent.getStringExtra("symbol"));
+    public StockIntentService() {
+        super(StockIntentService.class.getName());
     }
-    // We can call OnRunTask from the intent service to force it to run immediately instead of
-    // scheduling a task.
-    int result = stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
-    if(result == -1){
-      updateChart();
+
+    public StockIntentService(String name) {
+        super(name);
     }
-  }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mHandler = new Handler(Looper.getMainLooper());
+    }
+
+    public void updateChart() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                Toast toast = Toast.makeText(getBaseContext(), getString(R.string.symbol_not_available), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
+        StockTaskService stockTaskService = new StockTaskService(this);
+        Bundle args = new Bundle();
+        if (intent.getStringExtra("tag").equals("add")) {
+            args.putString("symbol", intent.getStringExtra("symbol"));
+        }
+        // We can call OnRunTask from the intent service to force it to run immediately instead of
+        // scheduling a task.
+        int result = stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+        if (result == -1) {
+            updateChart();
+        }
+    }
 }
